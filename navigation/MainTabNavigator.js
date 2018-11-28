31560a0ad6957f25c116c87da35bab0d94e3e6a2
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-28 17:14:47 +0900

    Done

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -99,7 +99,7 @@ export default createBottomTabNavigator(
     ProfileStack
   },
   {
-    initialRouteName: "ProfileStack",
+    initialRouteName: "HomeStack",
     tabBarOptions: {
       showLabel: false,
       style: {
